9d1eb8dc8264186870fb54eb49ed08544f3d87f3
hungnm0302 <coder.it0302@gmail.com>
2013-01-13 18:51:44 +0700

    Cập nhật sửa lỗi ngày 13/01/2013.

---
+++ www/sites/domua.khituongthuyvan.net/modules/datalogger/list_station_follow_province.js
@@ -0,0 +1,25 @@
+//thay doi gia tri tram do mưa theo tinh thanh quan ly
+(function ($){
+  $(document).ready(function(){
+    $('#edit-field-station-province-city-value').change( function () {
+			var filter = $(this).val();
+			// if(filter != 'All'){
+				var index = $('#edit-field-rainfall-station-nid').val();
+				console.log(index);
+				$('#edit-field-rainfall-station-nid').empty();
+				var tram = $('#edit-field-rainfall-station-nid');
+				if(tram.prop) {
+					var options = tram.prop('options');
+				}
+				else {
+					var options = tram.attr('options');
+				}
+				$.each(Drupal.settings.datalogger.list_station_province[filter], function(key, text) {
+					var nid = key.substr(4);
+					options[options.length] = new Option(text, nid);
+				});		
+				$('#edit-field-rainfall-station-nid').val(index);
+			// }
+		}).change();
+  });
+})(jQuery);
