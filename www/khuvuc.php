9b96c30c23659b033e12339c37a23e7004e76a84
trungtrungbo <dangthanh_0302@gmail.com>
2013-05-10 14:58:01 +0700

    khuvuc.php

--- www/khuvuc.php
+++ www/khuvuc.php
@@ -1,8 +1,22 @@
-﻿<?php 
+<?php 
 
   global $khuvuc;
-
+  global $admin_khuvuc;
+  
+  switch($_SERVER['SERVER_NAME']) {
+  case '113.160.225.111':
+    $khuvuc = array('mid_central_region','KHU VỰC TRUNG TRUNG BỘ');
+    $admin_khuvuc = array('khuvuctrungtrungbo', 'root');
+    break;
+  default:
+    die('Khong truy cap duoc phan mem Do mua theo ten mien nay. Hay lien he voi quan tri he thong de duoc ho tro.');
+  }
+  
   //$khuvuc = array('north_central_region','KHU VỰC BẮC TRUNG BỘ');
-  $khuvuc = array('mid_central_region','KHU VỰC TRUNG TRUNG BỘ');
+  
 
 
+  
+  
+  
+  
